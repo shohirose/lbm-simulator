2ab88f3660f06cd3854562e0a8004b73c09bc2e9
Sho Hirose <sho.hirose@gmail.com>
2023-03-22 23:48:25 +0900

    First commit

---
+++ include/lbm.hpp
@@ -0,0 +1,215 @@
+#ifndef LBM_HPP
+#define LBM_HPP
+
+#include <fmt/core.h>
+
+#include <Eigen/Core>
+#include <array>
+#include <cassert>
+#include <chrono>
+
+namespace lbm {
+
+struct Grid2d {
+  Grid2d(int nx_, int ny_) : nx{nx_}, ny{ny_} {}
+
+  Grid2d(const std::array<int, 2>& shape) : nx{shape[0]}, ny{shape[1]} {}
+
+  int nx;
+  int ny;
+
+  int size() const { return (nx + 2) * (ny + 2); }
+
+  int index(int i, int j) const {
+    assert(i >= -1 && i <= ny && "out of bounds error for index i!");
+    assert(j >= -1 && j <= nx && "out of bounds error for index j!");
+    return (i + 1) * (nx + 2) + j + 1;
+  }
+
+  int periodic_index(int i, int j) const {
+    if (i < -1) {
+      i += ny + 2;
+    }
+    if (j < -1) {
+      j += nx + 2;
+    }
+    if (i > ny) {
+      i -= ny + 2;
+    }
+    if (j > nx) {
+      j -= nx + 2;
+    }
+    assert(i >= -1 && i <= ny && "out of bounds error for index i!");
+    assert(j >= -1 && j <= nx && "out of bounds error for index j!");
+    return (i + 1) * (nx + 2) + j + 1;
+  }
+};
+
+struct Params {
+  std::array<int, 2> grid_shape;
+  std::array<double, 2> external_force;
+  double relaxation_time;
+  double error_limit;
+  int print_frequency;
+  int max_iter;
+};
+
+class LatticeBoltzmanMethodSimulator {
+ public:
+  LatticeBoltzmanMethodSimulator(const Params& params)
+      : grid_{params.grid_shape},
+        c_{},
+        w_{},
+        tau_{params.relaxation_time},
+        wcg_{},
+        error_limit_{params.error_limit},
+        print_freq_{params.print_frequency},
+        max_iter_{params.max_iter} {
+    // clang-format off
+    c_.transpose() <<  0,  0,
+                       1,  0,
+                       0,  1,
+                      -1,  0,
+                       0, -1,
+                       1,  1,
+                      -1,  1,
+                      -1, -1,
+                       1, -1;
+    w_ << 4.0 / 9.0,
+          1.0 / 9.0,  1.0 / 9.0,  1.0 / 9.0,  1.0 / 9.0,
+          1.0 / 36.0, 1.0 / 36.0, 1.0 / 36.0, 1.0 / 36.0;
+    // clang-format on
+
+    Eigen::Map<const Eigen::Vector2d> g(params.external_force.data());
+    wcg_ = w_.cwiseProduct(c_.transpose() * (3.0 * g));
+  }
+
+  Eigen::VectorXd run() const {
+    const auto size = grid_.size();
+    using Eigen::MatrixXd, Eigen::VectorXd;
+    using std::chrono::system_clock, std::chrono::duration_cast,
+        std::chrono::milliseconds;
+
+    // Initialization
+    MatrixXd u = MatrixXd::Zero(2, size);
+    MatrixXd u0 = u;
+    VectorXd rho = VectorXd::Ones(size);
+    MatrixXd feq = this->calc_equilibrium_distribution_function(u, rho);
+    MatrixXd f = feq;
+
+    double eps = 1.0;
+    int tsteps = 0;
+    const auto start = system_clock::now();
+
+    do {
+      tsteps += 1;
+      feq = this->calc_equilibrium_distribution_function(u, rho);
+      f = this->collision_process(f, feq, rho);
+      f = this->propagation_process(f);
+      this->apply_boundary_condition(f);
+      rho.noalias() = f.colwise().sum().transpose();
+      u.noalias() = (c_ * f).cwiseQuotient(rho.replicate<1, 2>().transpose());
+      eps = (u - u0).norm();
+      u0 = u;
+
+      if (tsteps % print_freq_ == 0) {
+        fmt::print("iter = {}, eps = {:.6e}\n", tsteps, eps);
+      }
+    } while (eps > error_limit_ && tsteps < max_iter_);
+
+    const auto end = system_clock::now();
+    const auto elapsed_time = duration_cast<milliseconds>(end - start).count();
+
+    fmt::print("total iter = {}, eps = {:.6e}, simulation time = {} sec\n",
+               tsteps, eps, elapsed_time * 1e-3);
+
+    return u;
+  }
+
+  template <typename T1, typename T2>
+  Eigen::MatrixXd calc_equilibrium_distribution_function(
+      const Eigen::MatrixBase<T1>& u, const Eigen::MatrixBase<T2>& rho) const {
+    using Eigen::MatrixXd;
+    MatrixXd feq(9, u.cols());
+    for (int i = 0; i < u.cols(); ++i) {
+      const Eigen::Matrix<double, 9, 1> cu = c_.transpose() * u.col(i);
+      feq.col(i).noalias() =
+          (w_.array() * rho(i) *
+           (1.0 + 3.0 * cu.array() + 4.5 * cu.array().square() -
+            1.5 * u.col(i).squaredNorm()))
+              .matrix();
+    }
+    return feq;
+  }
+
+  template <typename T1, typename T2, typename T3>
+  Eigen::MatrixXd collision_process(const Eigen::MatrixBase<T1>& f,
+                                    const Eigen::MatrixBase<T2>& feq,
+                                    const Eigen::MatrixBase<T3>& rho) const {
+    return f - (f - feq) / tau_ + wcg_ * rho.transpose();
+  }
+
+  template <typename T>
+  Eigen::MatrixXd propagation_process(const Eigen::MatrixBase<T>& f) const {
+    using Eigen::MatrixXd;
+    MatrixXd fnew = MatrixXd::Zero(f.rows(), f.cols());
+    for (int i = -1; i < grid_.ny + 1; ++i) {
+      for (int j = -1; j < grid_.nx + 1; ++j) {
+        const auto n = grid_.index(i, j);
+        fnew(0, n) = f(0, n);
+        fnew(1, grid_.periodic_index(i, j + 1)) = f(1, n);
+        fnew(2, grid_.periodic_index(i + 1, j)) = f(2, n);
+        fnew(3, grid_.periodic_index(i, j - 1)) = f(3, n);
+        fnew(4, grid_.periodic_index(i - 1, j)) = f(4, n);
+        fnew(5, grid_.periodic_index(i + 1, j + 1)) = f(5, n);
+        fnew(6, grid_.periodic_index(i + 1, j - 1)) = f(6, n);
+        fnew(7, grid_.periodic_index(i - 1, j - 1)) = f(7, n);
+        fnew(8, grid_.periodic_index(i - 1, j + 1)) = f(8, n);
+      }
+    }
+    return fnew;
+  }
+
+  template <typename T>
+  void apply_boundary_condition(Eigen::MatrixBase<T>& f) const {
+    // Bounce-back condition for bottom boundary
+    for (int j = -1; j < grid_.nx + 1; ++j) {
+      const auto n = grid_.index(-1, j);
+      f(2, n) = f(4, n);
+      f(5, n) = f(7, n);
+      f(6, n) = f(8, n);
+    }
+    // Bounce-back condition for top boundary
+    for (int j = -1; j < grid_.nx + 1; ++j) {
+      const auto n = grid_.index(grid_.ny, j);
+      f(4, n) = f(2, n);
+      f(7, n) = f(5, n);
+      f(8, n) = f(6, n);
+    }
+    // Periodic condition
+    for (int i = -1; i < grid_.ny + 1; ++i) {
+      const auto nl = grid_.index(i, -1);
+      const auto nr = grid_.index(i, grid_.nx);
+      f(1, nl) = f(1, nr);
+      f(3, nr) = f(3, nl);
+      f(5, nl) = f(5, nr);
+      f(6, nr) = f(6, nl);
+      f(7, nr) = f(7, nl);
+      f(8, nl) = f(8, nr);
+    }
+  }
+
+ private:
+  Grid2d grid_;
+  Eigen::Matrix<double, 2, 9> c_;
+  Eigen::Matrix<double, 9, 1> w_;
+  Eigen::Matrix<double, 9, 1> wcg_;
+  double tau_;
+  double error_limit_;
+  int print_freq_;
+  int max_iter_;
+};
+
+}  // namespace lbm
+
+#endif  // LBM_HPP
---
+++ src/poiseuille-flow-2d.cpp
@@ -0,0 +1,23 @@
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+
+#include "lbm.hpp"
+
+namespace fs = std::filesystem;
+
+int main() {
+  const std::array<int, 2> shape = {20, 20};
+  const std::array<double, 2> external_force = {0.00001, 0.0};
+  const double relaxation_time = 0.56;
+  const double error_limit = 1e-10;
+  const int print_freq = 5000;
+  const int max_iter = 1000000;
+  const lbm::Params params{shape,       external_force, relaxation_time,
+                           error_limit, print_freq,     max_iter};
+  lbm::LatticeBoltzmanMethodSimulator simulator(params);
+  const Eigen::VectorXd u = simulator.run();
+
+  std::ofstream file(fs::path("u.txt"));
+  file << u.transpose() << std::endl;
+}
