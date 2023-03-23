e98e1c7f02d0c32bfe5cfe3b46eeb2855cc0dc83
Sho Hirose <sho.hirose@gmail.com>
2023-03-23 21:59:16 +0900

    output x and y components of velocity seperately

--- src/poiseuille-flow-2d.cpp
+++ src/poiseuille-flow-2d.cpp
@@ -6,8 +6,11 @@
 
 namespace fs = std::filesystem;
 
+using Eigen::MatrixXd, Eigen::Map, Eigen::Stride, Eigen::Dynamic,
+    Eigen::Unaligned;
+
 int main() {
-  const std::array<int, 2> shape = {20, 20};
+  const std::array<int, 2> shape = {21, 21};
   const std::array<double, 2> external_force = {0.00001, 0.0};
   const double relaxation_time = 0.56;
   const double error_limit = 1e-10;
@@ -16,8 +19,21 @@ int main() {
   const lbm::Params params{shape,       external_force, relaxation_time,
                            error_limit, print_freq,     max_iter};
   lbm::PoiseuilleFlowSimulator simulator(params);
-  const Eigen::MatrixXd u = simulator.calc_velocity();
+  const MatrixXd u = simulator.calc_velocity();
 
-  std::ofstream file(fs::path("u.txt"));
-  file << u.transpose() << std::endl;
+  {
+    Map<const MatrixXd, Unaligned, Stride<Dynamic, 2>> ux(
+        &u(0, 0), shape[0], shape[1], Stride<Dynamic, 2>(shape[1]*2, 2));
+
+    std::ofstream file(fs::path("ux.txt"));
+    file << ux.transpose() << std::endl;
+  }
+
+  {
+    Map<const MatrixXd, Unaligned, Stride<Dynamic, 2>> uy(
+        &u(1, 0), shape[0], shape[1], Stride<Dynamic, 2>(shape[1]*2, 2));
+
+    std::ofstream file(fs::path("uy.txt"));
+    file << uy.transpose() << std::endl;
+  }
 }
