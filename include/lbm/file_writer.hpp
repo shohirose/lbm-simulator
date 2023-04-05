72c2af10bbe3ca4d913ceda691cb3d8263740035
Sho Hirose <sho.hirose@gmail.com>
2023-04-05 21:28:43 +0900

    simplify FileWriter

--- include/lbm/file_writer.hpp
+++ include/lbm/file_writer.hpp
@@ -4,16 +4,14 @@
 #include <fmt/core.h>
 
 #include <Eigen/Core>
-#include <cstdio>
 #include <filesystem>
 #include <fstream>
+#include <stdexcept>
 
 namespace lbm {
 
 class FileWriter {
  public:
-  FileWriter() : output_directory_{}, exists_{false} {}
-
   /**
    * @brief Construct a new File Writer object
    *
@@ -21,47 +19,34 @@ class FileWriter {
    * @throw std::filesystem::filesystem_error
    */
   FileWriter(const std::filesystem::path& output_directory)
-      : output_directory_{output_directory}, exists_{false} {
+      : output_directory_{output_directory} {
     namespace fs = std::filesystem;
     if (!fs::exists(output_directory)) {
       fs::create_directories(output_directory);
     }
-    exists_ = true;
-  }
-
-  void set_output_directory(const std::filesystem::path& output_directory) {
-    namespace fs = std::filesystem;
-    output_directory_ = output_directory;
-    fs::create_directories(output_directory);
-    exists_ = true;
   }
 
   /**
    * @brief Write matrix to file
    *
-   * @tparam T
    * @param x Matrix
    * @param filename File name
+   * @throw std::runtime_error When failed to open a file.
    */
   template <typename T>
-  void write(const Eigen::MatrixBase<T>& x,
-             const std::string& filename) const noexcept {
+  void write(const Eigen::MatrixBase<T>& x, const std::string& filename) const {
     namespace fs = std::filesystem;
-    if (!exists_) {
-      fs::create_directories(output_directory_);
+    const auto path = output_directory_ / fs::path(filename);
+    std::ofstream file(path);
+    if (!file) {
+      throw std::runtime_error(
+          fmt::format("Error: could not open a file: {}", path.string()));
     }
-    const auto p = output_directory_ / fs::path(filename);
-    std::ofstream f(p);
-    if (!f) {
-      fmt::print(stderr, "Error: could not open a file: {}", p.string());
-      std::exit(EXIT_FAILURE);
-    }
-    f << x << std::endl;
+    file << x << std::endl;
   }
 
  private:
   std::filesystem::path output_directory_;
-  bool exists_;
 };
 
 }  // namespace lbm
