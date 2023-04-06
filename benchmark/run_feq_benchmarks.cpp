736b80e1031883fa071834bcc59749062f4e8215
Sho Hirose <sho.hirose@gmail.com>
2023-04-06 21:56:16 +0900

    comment out cell-major layout benchmarks

--- benchmark/run_feq_benchmarks.cpp
+++ benchmark/run_feq_benchmarks.cpp
@@ -196,116 +196,116 @@ BENCHMARK_REGISTER_F(ComponentMajorLayoutFixture, FeqMethod8Test)
     ->RangeMultiplier(4)
     ->Range(64, 256 << 8);
 
-class CellMajorLayoutFixture : public ::benchmark::Fixture {
- public:
-  void SetUp(const ::benchmark::State& st) {
-    // clang-format off
-    c_.transpose() << 0,  1,  0, -1,  0,  1, -1, -1,  1,
-                      0,  0,  1,  0, -1,  1,  1, -1, -1;
-    w_ << 4.0 / 9.0,
-          1.0 / 9.0,  1.0 / 9.0,  1.0 / 9.0,  1.0 / 9.0,
-          1.0 / 36.0, 1.0 / 36.0, 1.0 / 36.0, 1.0 / 36.0;
-    // clang-format on
-    const auto n = st.range(0);
-    feq_ = Eigen::Matrix<double, Eigen::Dynamic, 9>::Zero(n, 9);
-    u_ = Eigen::Matrix<double, Eigen::Dynamic, 2>::Zero(n, 2);
-    rho_ = Eigen::VectorXd::Ones(n);
-  }
+// class CellMajorLayoutFixture : public ::benchmark::Fixture {
+//  public:
+//   void SetUp(const ::benchmark::State& st) {
+//     // clang-format off
+//     c_.transpose() << 0,  1,  0, -1,  0,  1, -1, -1,  1,
+//                       0,  0,  1,  0, -1,  1,  1, -1, -1;
+//     w_ << 4.0 / 9.0,
+//           1.0 / 9.0,  1.0 / 9.0,  1.0 / 9.0,  1.0 / 9.0,
+//           1.0 / 36.0, 1.0 / 36.0, 1.0 / 36.0, 1.0 / 36.0;
+//     // clang-format on
+//     const auto n = st.range(0);
+//     feq_ = Eigen::Matrix<double, Eigen::Dynamic, 9>::Zero(n, 9);
+//     u_ = Eigen::Matrix<double, Eigen::Dynamic, 2>::Zero(n, 2);
+//     rho_ = Eigen::VectorXd::Ones(n);
+//   }
 
-  void TearDown(const ::benchmark::State& st) {}
+//   void TearDown(const ::benchmark::State& st) {}
 
-  Eigen::Matrix<double, 9, 2> c_;
-  Eigen::Matrix<double, 9, 1> w_;
-  Eigen::Matrix<double, Eigen::Dynamic, 2> u_;
-  Eigen::VectorXd rho_;
-  Eigen::Matrix<double, Eigen::Dynamic, 9> feq_;
-};
+//   Eigen::Matrix<double, 9, 2> c_;
+//   Eigen::Matrix<double, 9, 1> w_;
+//   Eigen::Matrix<double, Eigen::Dynamic, 2> u_;
+//   Eigen::VectorXd rho_;
+//   Eigen::Matrix<double, Eigen::Dynamic, 9> feq_;
+// };
 
-BENCHMARK_DEFINE_F(CellMajorLayoutFixture, FeqMethod1Test)
-(benchmark::State& st) {
-  for (auto _ : st) {
-    for (int k = 0; k < 9; ++k) {
-      for (int i = 0; i < feq_.rows(); ++i) {
-        const auto u2 = u_.row(i).squaredNorm();
-        const auto cu = c_.row(k).dot(u_.row(i));
-        feq_(i, k) =
-            w_(k) * rho_(i) * (1.0 + 3.0 * cu + 4.5 * cu * cu - 1.5 * u2);
-      }
-    }
-  }
-}
+// BENCHMARK_DEFINE_F(CellMajorLayoutFixture, FeqMethod1Test)
+// (benchmark::State& st) {
+//   for (auto _ : st) {
+//     for (int k = 0; k < 9; ++k) {
+//       for (int i = 0; i < feq_.rows(); ++i) {
+//         const auto u2 = u_.row(i).squaredNorm();
+//         const auto cu = c_.row(k).dot(u_.row(i));
+//         feq_(i, k) =
+//             w_(k) * rho_(i) * (1.0 + 3.0 * cu + 4.5 * cu * cu - 1.5 * u2);
+//       }
+//     }
+//   }
+// }
 
-BENCHMARK_DEFINE_F(CellMajorLayoutFixture, FeqMethod2Test)
-(benchmark::State& st) {
-  for (auto _ : st) {
-    Eigen::Map<const Eigen::VectorXd> ux(&u_(0, 0), u_.rows());
-    Eigen::Map<const Eigen::VectorXd> uy(&u_(0, 1), u_.rows());
-    Eigen::Map<const Eigen::Matrix<double, 9, 1>> cx(&c_(0, 0));
-    Eigen::Map<const Eigen::Matrix<double, 9, 1>> cy(&c_(0, 1));
-    for (int k = 0; k < 9; ++k) {
-      for (int i = 0; i < feq_.rows(); ++i) {
-        const auto u2 = ux(i) * ux(i) + uy(i) * uy(i);
-        const auto cu = cx(k) * ux(i) + cy(k) * uy(i);
-        feq_(i, k) =
-            w_(k) * rho_(i) * (1.0 + 3.0 * cu + 4.5 * cu * cu - 1.5 * u2);
-      }
-    }
-  }
-}
+// BENCHMARK_DEFINE_F(CellMajorLayoutFixture, FeqMethod2Test)
+// (benchmark::State& st) {
+//   for (auto _ : st) {
+//     Eigen::Map<const Eigen::VectorXd> ux(&u_(0, 0), u_.rows());
+//     Eigen::Map<const Eigen::VectorXd> uy(&u_(0, 1), u_.rows());
+//     Eigen::Map<const Eigen::Matrix<double, 9, 1>> cx(&c_(0, 0));
+//     Eigen::Map<const Eigen::Matrix<double, 9, 1>> cy(&c_(0, 1));
+//     for (int k = 0; k < 9; ++k) {
+//       for (int i = 0; i < feq_.rows(); ++i) {
+//         const auto u2 = ux(i) * ux(i) + uy(i) * uy(i);
+//         const auto cu = cx(k) * ux(i) + cy(k) * uy(i);
+//         feq_(i, k) =
+//             w_(k) * rho_(i) * (1.0 + 3.0 * cu + 4.5 * cu * cu - 1.5 * u2);
+//       }
+//     }
+//   }
+// }
 
-BENCHMARK_DEFINE_F(CellMajorLayoutFixture, FeqMethod3Test)
-(benchmark::State& st) {
-  for (auto _ : st) {
-    Eigen::Map<const Eigen::VectorXd> ux(&u_(0, 0), u_.rows());
-    Eigen::Map<const Eigen::VectorXd> uy(&u_(0, 1), u_.rows());
-    Eigen::Map<const Eigen::Matrix<double, 9, 1>> cx(&c_(0, 0));
-    Eigen::Map<const Eigen::Matrix<double, 9, 1>> cy(&c_(0, 1));
-    const Eigen::VectorXd u2 =
-        (ux.array().square() + uy.array().square()).matrix();
-    for (int k = 0; k < 9; ++k) {
-      const Eigen::VectorXd cu = cx(k) * ux + cy(k) * uy;
-      feq_.col(k) = (w_(k) * rho_.array() *
-                     (1.0 + 3.0 * cu.array() + 4.5 * cu.array().square() -
-                      1.5 * u2.array()))
-                        .matrix();
-    }
-  }
-}
+// BENCHMARK_DEFINE_F(CellMajorLayoutFixture, FeqMethod3Test)
+// (benchmark::State& st) {
+//   for (auto _ : st) {
+//     Eigen::Map<const Eigen::VectorXd> ux(&u_(0, 0), u_.rows());
+//     Eigen::Map<const Eigen::VectorXd> uy(&u_(0, 1), u_.rows());
+//     Eigen::Map<const Eigen::Matrix<double, 9, 1>> cx(&c_(0, 0));
+//     Eigen::Map<const Eigen::Matrix<double, 9, 1>> cy(&c_(0, 1));
+//     const Eigen::VectorXd u2 =
+//         (ux.array().square() + uy.array().square()).matrix();
+//     for (int k = 0; k < 9; ++k) {
+//       const Eigen::VectorXd cu = cx(k) * ux + cy(k) * uy;
+//       feq_.col(k) = (w_(k) * rho_.array() *
+//                      (1.0 + 3.0 * cu.array() + 4.5 * cu.array().square() -
+//                       1.5 * u2.array()))
+//                         .matrix();
+//     }
+//   }
+// }
 
-BENCHMARK_DEFINE_F(CellMajorLayoutFixture, FeqMethod4Test)
-(benchmark::State& st) {
-  for (auto _ : st) {
-    Eigen::Map<const Eigen::VectorXd> ux(&u_(0, 0), u_.rows());
-    Eigen::Map<const Eigen::VectorXd> uy(&u_(0, 1), u_.rows());
-    Eigen::Map<const Eigen::Matrix<double, 9, 1>> cx(&c_(0, 0));
-    Eigen::Map<const Eigen::Matrix<double, 9, 1>> cy(&c_(0, 1));
-    const Eigen::VectorXd u2 =
-        (ux.array().square() + uy.array().square()).matrix();
-    for (int k = 0; k < 9; ++k) {
-      const Eigen::VectorXd cu = cx(k) * ux + cy(k) * uy;
-      feq_.col(k) = (w_(k) * rho_.array() *
-                     (1.0 + 3.0 * (cx(k) * ux + cy(k) * uy).array() +
-                      4.5 * (cx(k) * ux + cy(k) * uy).array().square() -
-                      1.5 * u2.array()))
-                        .matrix();
-    }
-  }
-}
+// BENCHMARK_DEFINE_F(CellMajorLayoutFixture, FeqMethod4Test)
+// (benchmark::State& st) {
+//   for (auto _ : st) {
+//     Eigen::Map<const Eigen::VectorXd> ux(&u_(0, 0), u_.rows());
+//     Eigen::Map<const Eigen::VectorXd> uy(&u_(0, 1), u_.rows());
+//     Eigen::Map<const Eigen::Matrix<double, 9, 1>> cx(&c_(0, 0));
+//     Eigen::Map<const Eigen::Matrix<double, 9, 1>> cy(&c_(0, 1));
+//     const Eigen::VectorXd u2 =
+//         (ux.array().square() + uy.array().square()).matrix();
+//     for (int k = 0; k < 9; ++k) {
+//       const Eigen::VectorXd cu = cx(k) * ux + cy(k) * uy;
+//       feq_.col(k) = (w_(k) * rho_.array() *
+//                      (1.0 + 3.0 * (cx(k) * ux + cy(k) * uy).array() +
+//                       4.5 * (cx(k) * ux + cy(k) * uy).array().square() -
+//                       1.5 * u2.array()))
+//                         .matrix();
+//     }
+//   }
+// }
 
-BENCHMARK_REGISTER_F(CellMajorLayoutFixture, FeqMethod1Test)
-    ->RangeMultiplier(4)
-    ->Range(64, 256 << 8);
+// BENCHMARK_REGISTER_F(CellMajorLayoutFixture, FeqMethod1Test)
+//     ->RangeMultiplier(4)
+//     ->Range(64, 256 << 8);
 
-BENCHMARK_REGISTER_F(CellMajorLayoutFixture, FeqMethod2Test)
-    ->RangeMultiplier(4)
-    ->Range(64, 256 << 8);
+// BENCHMARK_REGISTER_F(CellMajorLayoutFixture, FeqMethod2Test)
+//     ->RangeMultiplier(4)
+//     ->Range(64, 256 << 8);
 
-BENCHMARK_REGISTER_F(CellMajorLayoutFixture, FeqMethod3Test)
-    ->RangeMultiplier(4)
-    ->Range(64, 256 << 8);
+// BENCHMARK_REGISTER_F(CellMajorLayoutFixture, FeqMethod3Test)
+//     ->RangeMultiplier(4)
+//     ->Range(64, 256 << 8);
 
-BENCHMARK_REGISTER_F(CellMajorLayoutFixture, FeqMethod4Test)
-    ->RangeMultiplier(4)
-    ->Range(64, 256 << 8);
+// BENCHMARK_REGISTER_F(CellMajorLayoutFixture, FeqMethod4Test)
+//     ->RangeMultiplier(4)
+//     ->Range(64, 256 << 8);
 
 BENCHMARK_MAIN();
